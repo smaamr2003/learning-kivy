f8680aa0b08be1080ee7e2e00c927add84918762
Rakib Abdullah <smaamr2003@gmail.com>
2023-10-28 19:05:49 +0600

    Buttons and Labels

--- main.py
+++ main.py
@@ -4,16 +4,19 @@ from kivy.uix.widget import Widget
 
 class MainWidget(Widget):
     pass
-    # The root rule in kv file is declared by 
-    # declaring the class of your root widget, 
-    # without any 
-    # indentation, followed by : and will be set 
-    # as the root attribute of the App instance:
+    #'dp' stands for density-independent pixels
+    # It could be possible that we set size 
+    # from one device, which is larger or 
+    # smaller in another device. For this, dp is 
+    # used so that the size adjusts to each device
+
+    # The values in 'color' are float numbers 
+    # from 0 to 1, indicating the percentage of 
+    # each color red, green, blue. Last value 
+    # stands for the opacity of the text
+    
 
 class TheLabApp(App):
     pass
-    # In this app, we can set the widgets without 
-    # needing to use the kv file (using the 'build' 
-    # method), but using kv file is much easier
     
 TheLabApp().run()
